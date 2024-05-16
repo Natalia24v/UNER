1ce20ebed52a6c422a6a7884c0be7b53c9852861
NatG <101484974+Natalia24v@users.noreply.github.com>
2024-05-16 15:54:05 -0300

    excercise 5 completed

--- TP2/TP2EJ5Nat_G.py
+++ TP2/TP2EJ5Nat_G.py
@@ -1,9 +1,16 @@
 #5. Que dado tres números ingresados por el usuario nos devuelva el promedio.
 
-num1 = float(input("Ingrese un numero: "))
-num2 = float(input("Ingrese otro numero: "))
-num3 = float(input("Ingrese un tercer numero: "))
+#Se define la función promedio
+def promedio():
+    #Se pide al usuario que ingrese tres números que se convierten a float
+    num1 = float(input("Ingrese un numero: "))
+    num2 = float(input("Ingrese otro numero: "))
+    num3 = float(input("Ingrese un tercer numero: "))
 
-resultado = (num1 + num2 + num3) / 3.0
+    #se calcula el promedio de los numeros ingresados
+    resultado = (num1 + num2 + num3) / 3.0
+    #Se imprime el promedio por consola
+    print("El resultado es: ", str(resultado))
 
-print("El resultado es: ", resultado)
+#Se invoca a la función para su ejecución
+promedio()
