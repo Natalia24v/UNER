db3310471088285db222112ba39e3ac25b049956
NatG <101484974+Natalia24v@users.noreply.github.com>
2024-05-16 16:08:22 -0300

    add function to excercise 2.7

--- TP2/TP2EJ7Nat_G.py
+++ TP2/TP2EJ7Nat_G.py
@@ -1,4 +1,11 @@
 #7. Que imprima todos los números entre el 100 y el 199.
 
-for i in range(100, 200):
-    print(i)
+#Se inicia la funcion rangoCien que imprimirá el rango de números del 100 al 199
+def rangoCien():
+    #Se inicia un bucle for con el rango entre 100 y 200 ya que comienza el contador en 0
+    for i in range(100, 200):
+        #Se imprime el valor del contador por consola
+        print(i)
+
+#se llama a la funcion rangoCien para su ejecucción
+rangoCien()
