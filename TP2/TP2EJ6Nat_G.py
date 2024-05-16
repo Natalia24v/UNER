08f01b0d08c33926e44b6a66dfdc5ab19e904988
NatG <101484974+Natalia24v@users.noreply.github.com>
2024-05-16 16:00:30 -0300

    add comments to excercise 2.6

--- TP2/TP2EJ6Nat_G.py
+++ TP2/TP2EJ6Nat_G.py
@@ -2,6 +2,10 @@
 #(y en lo posible y dado el caso funciones) para escribir programas que lleven a cabo lo siguiente:
 #6. Que pida al usuario una palabra y la muestre por pantalla 10 veces.
 
+#Se pide al usuario que ingrese una palabra.
 palabra = input("Ingrese una palabra: ")
+
+# Se inicia un bucle for que se ejecutará 10 veces. La función range(10) genera una secuencia de números del 0 al 9.
 for i in range(10):
+    # En cada iteración se imprime la palabra que el usuario ingresó, dando untotal de 10 veces
     print(palabra)
